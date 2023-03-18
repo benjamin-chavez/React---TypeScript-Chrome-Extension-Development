a7b34df05e21069fa4b27e73846a179f4fff765f
Ben Chavez <ben.m.chavez@gmail.com>
2023-03-18 16:22:14 -0500

    updated popup to hold a search bar and refactored popup code

--- Section 6: Advanced Project - Weather Extension/Weather Extension/src/popup/popup.tsx
+++ Section 6: Advanced Project - Weather Extension/Weather Extension/src/popup/popup.tsx
@@ -1,16 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
+import {
+  Box,
+  Grid,
+  InputBase,
+  IconButton,
+  Paper,
+  Input,
+} from '@material-ui/core';
+import { Add, Add as AddIcon } from '@material-ui/icons';
 import '@fontsource/roboto';
 import './popup.css';
 import WeatherCard from './WeatherCard';
 
 const App: React.FC<{}> = () => {
+  const [cities, setCities] = useState<string[]>([
+    'Miami',
+    'Barcelona',
+    'Error',
+  ]);
+
   return (
-    <div>
-      <WeatherCard city="Miami" />
-      <WeatherCard city="Barcelona" />
-      <WeatherCard city="New York" />
-    </div>
+    <Box mx="8px" my="16px">
+      <Grid container>
+        <Grid item>
+          <Paper>
+            <Box px="15px" py="5px">
+              <InputBase placeholder="Add a city name" />
+              <IconButton>
+                <AddIcon />
+              </IconButton>
+            </Box>
+          </Paper>
+        </Grid>
+      </Grid>
+      {cities.map((city, index) => (
+        <WeatherCard city={city} key={index} />
+      ))}
+    </Box>
   );
 };
 
