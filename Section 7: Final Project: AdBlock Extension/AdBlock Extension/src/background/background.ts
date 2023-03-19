309ac7e4ec99cd88e860279d583613ec7c98a771
Ben Chavez <ben.m.chavez@gmail.com>
2023-03-19 15:19:41 -0500

    basic adblocker extension

---
+++ Section 7: Final Project: AdBlock Extension/AdBlock Extension/src/background/background.ts
@@ -0,0 +1,16 @@
+chrome.webRequest.onBeforeRequest.addListener(
+  (details) => {
+    console.log(details);
+    return {
+      cancel: true,
+    };
+  },
+  {
+    urls: [
+      '*://*.googleadservices.com/*',
+      '*://*.tpc.googlesyndication.com/*',
+      '*://*.g.doubleclick.net/*',
+    ],
+  },
+  ['blocking']
+);
