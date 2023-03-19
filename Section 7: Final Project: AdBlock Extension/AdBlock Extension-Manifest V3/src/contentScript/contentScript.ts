033a2bd30ec439442277c60a5bb97a9f113dee68
Ben Chavez <ben.m.chavez@gmail.com>
2023-03-19 16:46:40 -0500

    remove ads using DOM manipulation. Again, couldn't get this one working either

--- Section 7: Final Project: AdBlock Extension/AdBlock Extension-Manifest V3/src/contentScript/contentScript.ts
+++ Section 7: Final Project: AdBlock Extension/AdBlock Extension-Manifest V3/src/contentScript/contentScript.ts
@@ -1,1 +1,42 @@
-// TODO: content script
+const rules: {
+  [url: string]: () => void;
+} = {
+  // 'https://www.nytimes.com/': filterNYTTechnology,
+  'https://www.nytimes.com/': filterNYTTechnology2,
+};
+
+function filterNYTTechnology() {
+  return;
+
+  const app = document.getElementById('site-content');
+  // const wrapper = document.getElementById('top-wrapper');
+  const wrapper = document.getElementById('dfp-ad-top');
+
+  app.removeChild(wrapper);
+}
+
+function filterNYTTechnology2() {
+  const divs = document.getElementsByTagName('div');
+
+  for (const div of divs) {
+    if (div.className.indexOf('ad') != -1) {
+      div.style.display = 'none';
+    }
+  }
+}
+
+if (document.URL in rules) {
+  console.log(document.URL);
+  rules[document.URL]();
+}
+
+// const intervalId = setInterval(() => {
+//   const app = document.getElementById('site-content');
+//   const wrapper = document.getElementById('dfp-ad-top');
+
+//   // if app and wrapper elements exist, remove DOM node and clear timer
+//   if (app && wrapper) {
+//     app.removeChild(wrapper[0]);
+//     clearInterval(intervalId);
+//   }
+// }, 100);
